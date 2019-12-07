a831f5faaa009de286778971f2d5cd29d37bb68b
Cornejo22 <miguel.cornejo.manzanares@gmail.com>
2019-12-07 12:24:49 +0100

    I added new changes

--- CartaBaraja/app/src/main/java/com/example/cartabaraja/Baraja.kt
+++ CartaBaraja/app/src/main/java/com/example/cartabaraja/Baraja.kt
@@ -1,23 +1,48 @@
 package com.example.cartabaraja
 
+import androidx.annotation.ArrayRes
 import java.lang.Exception
+import kotlin.random.Random
 
 class Baraja {
 
     //Atributos
-    private var listaCartas: MutableCollection<Carta>
+    private var listaCartas: ArrayList<Carta>
 
     //Constructor
-    constructor (){
-       this.listaCartas = mutableSetOf()
-    }
+//    constructor(){
+//       this.listaCartas = ArrayList()
+//    }
 
     constructor(tipoBaraja: Int){
-        this.listaCartas = mutableSetOf()
+        this.listaCartas = ArrayList()
+        this.listaCartas = crearBaraja(tipoBaraja)
+
+    }
+
+    constructor(tipoBaraja: Int,barajar:Boolean){
+        this.listaCartas = crearBaraja(tipoBaraja)
+
+        if(barajar === true){
+            barajamos()
+        }
+
+    }
+
+    fun recorrer(){
+        var i:Int = 0
+        for (i in this.listaCartas){
+            println(i.nombreCarta())
+        }
+    }
+
+    fun crearBaraja(tipoBaraja: Int):ArrayList<Carta>{
+      var listaDeCartas:ArrayList<Carta> = ArrayList()
 
         if(tipoBaraja == 1){
             for (i in 1..40){
                 var carta = Carta(i)
+                println(carta.nombreCarta())
                 this.listaCartas.add(carta)
             }
         }
@@ -36,10 +61,18 @@ class Baraja {
                 throw Exception("Error en el tipo de baraja")
             }
         }
+
+      return listaDeCartas
     }
 
-    constructor(tipoBaraja: Int,barajar:Boolean){
-        this.listaCartas = mutableSetOf()
-    }
+    fun barajamos(){
+        var listTemporal:ArrayList<Carta> = ArrayList()
+        var random:Random = Random
+        var pos:Int
 
+        while (this.listaCartas.size > 0){
+            pos = random.nextInt(this.listaCartas.size)
+            listTemporal.add(this.listaCartas.get(pos))
+        }
+    }
 }
